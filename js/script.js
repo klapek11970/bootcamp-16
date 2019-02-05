8d9304576d2746bb443c7bc32cbc647b0db252bc
Patryk Zahorski <maarcane@gmail.com>
2019-02-05 12:53:33 +0100

    fix https in js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ getJoke()
 var paragraph = document.getElementById("Joke")
 function getJoke() {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://api.icndb.com/jokes/random');
+  xhr.open('GET', 'https://api.icndb.com/jokes/random');
   xhr.addEventListener('load', function(){
     var response = JSON.parse(xhr.response);
     paragraph.innerHTML = response.value.joke;
