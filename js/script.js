8ff1318ee5aa9c61f8ebdc00565f81f2f5dd1afc
Patryk Zahorski <maarcane@gmail.com>
2019-02-05 12:34:50 +0100

    16.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+getJoke()
+
+var paragraph = document.getElementById("Joke")
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://api.icndb.com/jokes/random');
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
