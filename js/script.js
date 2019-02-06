8edba0300fd4e3b0f97056a236d8161d29f36f81
Patryk Zahorski <maarcane@gmail.com>
2019-02-06 15:23:49 +0100

    fix quote on page load

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 'use strict';
-getJoke()
 
 var quote = document.getElementById("quote")
 var author = document.getElementById("author")
+var tweetbutton = document.getElementById("tweet")
 var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = prefix+"https://twitter.com/intent/tweet?text=";
 var quoteUrl = prefix+"https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 var url = 'https://restcountries.eu/rest/v2/name/';
 var link = ""
 
-
 var countriesList = document.getElementById('countries');
 document.getElementById('search').addEventListener('click', searchCountries);
+
+getQuote()
 
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
@@ -48,6 +49,7 @@ function showCountriesList(resp) {
 }
 
 document.getElementById("joke").addEventListener('click', function(){
+	tweetbutton.disabled = true
 	getJoke()
 })
 
@@ -55,7 +57,7 @@ document.getElementById("twquote").addEventListener('click', function(){
 	getQuote()
 })
 
-document.getElementById("tweet").addEventListener('click', function(){
+tweetbutton.addEventListener('click', function(){
 	if(link){
 		location.href=link
 	}
@@ -63,11 +65,12 @@ document.getElementById("tweet").addEventListener('click', function(){
 
 
 function getQuote() {
-    fetch(quoteUrl, { cache: "no-store" })
-        .then(function(resp) {
-            return resp.json();
-        })
-        .then(createTweet);
+	tweetbutton.disabled = true
+	fetch(quoteUrl, { cache: "no-store" })
+			.then(function(resp) {
+					return resp.json();
+			})
+			.then(createTweet);
 }
 
 function createTweet(input) {
@@ -78,9 +81,9 @@ function createTweet(input) {
     var quoteText = dataElement.innerText.trim();
 		
 		if (quoteText.length > 140) {
-				getQuote();
-				console.log(quoteText,quoteText.length)
-				return
+			getQuote();
+			console.log(quoteText,quoteText.length)
+			return
 		}
 		
     var quoteAuthor = data.title || "Unknown author";
@@ -90,6 +93,7 @@ function createTweet(input) {
 		quote.innerText = "Quote of the day - " + quoteText
 		author.innerText = quoteAuthor
 		link = data.link
+		tweetbutton.disabled = false
 }
 
 function getJoke() {
